90249964853e4819a12f9f0beae1b491282c050f
Gerardo Urzua <gerardourzua20@gmail.com>
2024-07-08 11:31:14 -0700

    Start8

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,6 @@
 export const ENV = {
-  /* SERVER_HOST: 'https://minegocioenlinea-strapi.onrender.com',
-  API_URL: 'https://minegocioenlinea-strapi.onrender.com/api', */
-  SERVER_HOST: 'http://localhost:1337',
-  API_URL: 'http://localhost:1337/api', 
+  SERVER_HOST: 'https://minegocioenlinea-strapi.onrender.com',
+  API_URL: 'https://minegocioenlinea-strapi.onrender.com/api', 
   ENDPOINTS: {
     AUTH: {
       REGISTER: 'auth/local/register',
